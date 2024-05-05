0f36b00b7dc8ea570a878446e897de6411d5c5fd
Aman Kumar Rai <tej171995@gmail.com>
2024-05-05 18:55:46 +0530

    fixed bug

--- app.js
+++ app.js
@@ -139,7 +139,9 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(7000, () => {
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
   console.log("Serving on port 7000");
 });
 
