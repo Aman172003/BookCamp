ee040543e5fd1a7f9c7c24e9d4e07681a5dae68a
Aman Kumar Rai <tej171995@gmail.com>
2024-05-09 11:19:07 +0530

    dbaws

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
 const mysql = require("mysql2/promise");
 
+// const mySqlPool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "Aman@172003",
+//   database: "bookcamp",
+// });
+
 const mySqlPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Aman@172003",
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
   database: "bookcamp",
 });
 
